72691603e2fe800b3f451e4c6cf653e8b03641bd
Bruno Umbelino <bruno.umbelino0@gmail.com>
2018-05-23 16:42:20 -0300

    DataAnnotation

---
+++ MeuEstoque/Migrations/201805231936175_dataannotation.cs
@@ -0,0 +1,16 @@
+namespace MeuEstoque.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class dataannotation : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- MeuEstoque/Models/Classes/Produto.cs
+++ MeuEstoque/Models/Classes/Produto.cs
@@ -13,17 +13,23 @@ namespace MeuEstoque.Models.Classes
         [Column(Order = 0)]
         public int Id { get; set; }
 
-        [Required(ErrorMessage ="*")]
+        [Required(ErrorMessage = "*")]
+        [MinLength(5, ErrorMessage = "Mínimo 5 caracteres")]
+        [MaxLength(25, ErrorMessage = "Máximo 25 caracteres")]
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "*")]
+        [DisplayFormat(DataFormatString ="{0:n3}")]
+        [Display(Name ="Peso (Gramas)")]
         public float Peso { get; set; }
 
         [Required(ErrorMessage = "*")]
+        [Range(1,500,ErrorMessage ="Quantidade mínima de 1 e máxima de 500")]
         public int Quantidade { get; set; }
 
         [Required(ErrorMessage = "*")]
         [Display(Name = "Preço Unitário")]
+        [DataType(DataType.Currency)]
         public double PrecoUnitario { get; set; }
 
         [Required(ErrorMessage = "*")]
